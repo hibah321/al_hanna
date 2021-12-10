73be3768c7952644e1d8ff100d6ec28a3c77591c
hibah321 <hbhshahbaz321@gmail.com>
2021-12-10 23:58:46 +0400

    f

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-document.getElementById("submit").onsubmit((e) =>{
-    e.preventDefault();
-    
+document.getElementById("submit").addEventListener('submit' , (e) =>{
+    e.preventDefault()
 })
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-links");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
