dd134dbc62ffa7149af0953921b54bac22a079de
hibah321 <hbhshahbaz321@gmail.com>
2021-12-11 23:37:58 +0400

    fa

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-links");
 const body = document.querySelector("body");
+const html = document.querySelector("html");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     body.classList.toggle("lock-scroll");
+    html.classList.toggle("lock-scroll");
 }
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
