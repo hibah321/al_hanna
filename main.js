3f5cb8ae8aacd31492cbfecf2952f93319e450ce
hibah321 <hbhshahbaz321@gmail.com>
2021-12-15 20:59:28 +0400

    final

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-document.getElementById("submit").addEventListener('submit' , (e) =>{
-    e.preventDefault()
-})
+
+const form = document.querySelectorAll("#for");
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-links");
 const body = document.querySelector("body");
 const html = document.querySelector("html");
+
+document.getElementById("submit").addEventListener('submit' , (e) =>{
+    e.preventDefault();
+    
+})
+form.forEach((e) =>{
+    e.reset();
+})
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
@@ -20,5 +27,15 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
+}
+function getFile() {
+    document.getElementById("upfile").click();
+  }
+function sub(obj) {
+    var file = obj.value;
+    var fileName = file.split("\\");
+    document.getElementById("yourBtn").innerHTML = fileName[fileName.length - 1];
+    document.myForm.submit();
+    event.preventDefault();
 }
 
